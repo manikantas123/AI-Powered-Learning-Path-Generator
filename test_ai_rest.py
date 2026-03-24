0804ac027c3a0f22eca73f852daefab061dbc029
Manikanta <manikantamummidi5@gmail.com>
2026-03-25 01:02:12 +0530

    text change

--- test_ai_rest.py
+++ test_ai_rest.py
@@ -4,4 +4,6 @@ headers = {'Content-Type': 'application/json'}
 payload = {"contents": [{"parts": [{"text": "Hello"}]}]}
 res = requests.post(url, headers=headers, json=payload)
 print(res.status_code)
+
+
 print(res.text)
